c13956d4ade744f84d441587517b9a39bc9ffbe3
Javier Cazallas <jcazallasc@gmail.com>
2020-05-22 10:45:26 +0200

    Add create and delete

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -17,7 +17,14 @@ Todos List
                     @foreach($todos as $todo)
                         <li class="list-group-item">
                             {{ $todo->name }}
-                            <a href="/todos/{{ $todo->id }}" class="btn btn-primary btn-sm float-right">
+                            <form action="/delete-todo/{{ $todo->id }}" method="post">
+                                @csrf
+                                @method('delete')
+                                <button type="submit" class="btn btn-danger btn-sm float-right">
+                                    Delete
+                                </button>
+                            </form>
+                            <a href="/todos/{{ $todo->id }}" class="btn btn-primary btn-sm float-right mr-1">
                                     View
                             </a>
                         </li>
--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,6 @@ Route::get('/', function () {
 
 Route::get('todos', 'TodosController@index');
 Route::get('todos/{todo}', 'TodosController@show');
+Route::get('new-todos', 'TodosController@create');
+Route::post('store-todos', 'TodosController@store');
+Route::delete('delete-todo/{todo}', 'TodosController@delete');
