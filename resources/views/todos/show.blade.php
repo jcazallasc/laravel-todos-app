5946b297b6820bb749b9bf31baca1dd953395c95
Javier Cazallas <jcazallasc@gmail.com>
2020-05-22 10:19:06 +0200

    Add layout for common code

--- resources/views/todos/show.blade.php
+++ resources/views/todos/show.blade.php
@@ -1,30 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" crossorigin="anonymous">
-    <title>Todos Item</title>
-</head>
-<body>
-    <div class="container">
-        <h1 class="text-center my-5">{{ $todo->name }}</h1>
+@extends('layouts.app')
 
-        <div class="row justify-content-center">
-            <div class="col-md-6">
-                <div class="card card-default">
-                    <div class="card-header">
-                        Details
-                    </div>
-                    <div class="card-body">
-                        {{ $todo->description }}
-                    </div>
-                </div>
+@section('content')
+<h1 class="text-center my-5">{{ $todo->name }}</h1>
+
+<div class="row justify-content-center">
+    <div class="col-md-6">
+        <div class="card card-default">
+            <div class="card-header">
+                Details
+            </div>
+            <div class="card-body">
+                {{ $todo->description }}
             </div>
         </div>
-
-        
     </div>
-    
-</body>
-</html>
+</div>
+@endsection
