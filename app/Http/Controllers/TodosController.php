0d2c2f4605d5df5ff434789c092a3c62092eb9a2
Javier Cazallas <jcazallasc@gmail.com>
2020-05-22 10:52:41 +0200

    Add validation for creating

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -24,6 +24,10 @@ class TodosController extends Controller
 
     public function store()
     {
+        $this->validate(request(), [
+            'name' => 'required|min:6|max:255',
+            'description' => 'required',
+        ]);
         $data = request()->all();
         $todo = new Todo();
         $todo->name = $data['name'];
--- resources/views/todos/create.blade.php
+++ resources/views/todos/create.blade.php
@@ -12,13 +12,22 @@ Create Todos
         <div class="card card-default">
             <div class="card-header">Create new todo</div>
             <div class="card-body">
+                @if($errors->any())
+                    <div class="div alert alert-danger">
+                        <ul class="li list-group">
+                            @foreach($errors->all() as $error)
+                        <li class="list-group-item">{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
                 <form action="/store-todos" method="POST">
                     @csrf
                     <div class="form-group">
-                        <input type="text" class="form-control" name="name"/>
+                        <input type="text" class="form-control" name="name" placeholder="Name"/>
                     </div>
                     <div class="form-group">
-                        <textarea name="description" placeholder="Description" cols="50" rows="10"></textarea>
+                        <textarea class="form-control" name="description" placeholder="Description" cols="5" rows="5"></textarea>
                     </div>
                     <div class="form-group text-center">
                         <button type="submit" class="btn btn-success">Create Todo</button>
