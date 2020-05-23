6a9c6eec85489844d7641d3b4949a9b1bb1d347e
Javier Cazallas <jcazallasc@gmail.com>
2020-05-23 09:47:07 +0200

    Allow massive assignable

--- app/Todo.php
+++ app/Todo.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Todo extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['name', 'description', 'completed'];
+
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = [];
 }
